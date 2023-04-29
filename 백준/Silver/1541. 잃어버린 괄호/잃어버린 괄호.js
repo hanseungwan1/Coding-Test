358c7141bec45130064bfd561779dcc056fc6ad6
hanseungwan1 <115906460+hanseungwan1@users.noreply.github.com>
2023-04-29 15:02:29 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 132 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().split("\n");
+const data = input[0].split("-");
+let answer = 0;
+for (let i = 0; i < data.length; i++) {
+  let cur = data[i]
+    .split("+")
+    .map(Number)
+    .reduce((a, b) => a + b);
+  if (i == 0) answer += cur;
+  else answer -= cur;
+}
+console.log(answer);
