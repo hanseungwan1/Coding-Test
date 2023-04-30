f7b7d47a850be75ec3bcf56a2f0945224adaf368
hanseungwan1 <115906460+hanseungwan1@users.noreply.github.com>
2023-04-30 15:18:10 +0900

    [Silver I] Title: 회의실 배정, Time: 404 ms, Memory: 42928 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().split("\n");
+const n = Number(input[0]);
+let arr = [];
+for (let i = 1; i < n + 1; i++) arr.push(input[i].split(" ").map(Number));
+
+arr.sort((a, b) => {
+  if (a[1] != b[1]) return a[1] - b[1];
+  else return a[0] - b[0];
+});
+
+let count = 1;
+let cur = 0;
+for (let i = 1; i < n; i++) {
+  if (arr[cur][1] <= arr[i][0]) {
+    count++;
+    cur = i;
+  }
+}
+console.log(count);
