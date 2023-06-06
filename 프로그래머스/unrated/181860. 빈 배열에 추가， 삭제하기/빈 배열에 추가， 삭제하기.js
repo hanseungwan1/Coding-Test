de9427e23548b4af9f1af20a1f72b016c345c79f
hanseungwan1 <115906460+hanseungwan1@users.noreply.github.com>
2023-06-06 22:06:48 +0900

    [unrated] Title: 빈 배열에 추가, 삭제하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -1,15 +1,8 @@
 function solution(arr, flag) {
   let result = [];
   flag.forEach((el, index) => {
-    if (el) {
-      for (let i = 0; i < arr[index] * 2; i++) {
-        result.push(arr[index]);
-      }
-    } else {
-      for (let i = 0; i < arr[index]; i++) {
-        result.pop();
-      }
-    }
+    if (el) for (let i = 0; i < arr[index] * 2; i++) result.push(arr[index]);
+    else for (let i = 0; i < arr[index]; i++) result.pop();
   });
   return result;
 }
